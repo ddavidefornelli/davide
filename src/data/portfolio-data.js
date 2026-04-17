ead528976051fd52dcf871123d3436f12966e3f0
ddavidefornelli <fornelli.dv@gmail.com>
2026-04-17 22:14:18 +0200

    added some projects

--- src/data/portfolio-data.js
+++ src/data/portfolio-data.js
@@ -1,17 +1,27 @@
 export const projectCollection = [
   {
     title: "Sudoku",
-    description: "Terminal Sudoku game in C",
+    description: "the classic sudoku game played in the terminal made in C.",
     href: "https://github.com/ddavidefornelli/sudoku",
-  },
-  {
-    title: "Boid flock",
-    description: "Boids algorithm with interactive controls",
-    href: "https://github.com/ddavidefornelli/bird-flock",
   },
   {
     title: "No Screenshot",
     description: "A word you can read but can't screenshot",
     href: "",
+  },
+  {
+    title: "Mega Tris",
+    description: "my first project made in react, and also the first project where I used git. the code is shit but i'm still proud of it.",
+    href: "https://github.com/ddavidefornelli/megatris",
+  },
+  {
+    title: "Boid flock",
+    description: "It just looks cool my g.",
+    href: "https://github.com/ddavidefornelli/bird-flock",
+  },
+  {
+    title: "Dot files",
+    description: "All of my nerdy linux and nvim minimal configurations. I use arch btw.",
+    href: "https://github.com/ddavidefornelli/dotfiles",
   },
 ];
